b6508526086b16aac3160a1f2ad9919d66b177b8
danigb <danigb@gmail.com>
2015-06-26 17:17:21 +0200

    First complete implementation

--- index.js
+++ index.js
@@ -1,19 +1,64 @@
-var Note = require('note-pitch')
 
-module.exports = Escalas
+module.exports = Scale
 
-function Escalas () {
-  return
+function Scale (number) {
+  if (!(this instanceof Scale)) return new Scale(number)
+
+  if (/^\d+$/.test(number)) {
+    this.binary = number.toString(2)
+  } else {
+    this.binary = number
+  }
+
+  if (!/^1[01]*$/.test(this.binary)) {
+    throw Error('Wrong scale number: ' + number + ' (' + this.binary + ')')
+  }
 }
 
 var INTERVALS = ['P1', 'm2', 'M2', 'm3', 'M3', 'P4', 'd5', 'P5', 'm6', 'M6', 'm7', 'M7']
-
-Escalas.build = function (name, root) {
-  return Note.transpose(root, Escalas.intervals(name))
-}
-Escalas.intervals = function (name) {
-  return name.toString(2).split('').reduce(function (intervals, val, index) {
+Scale.prototype.intervals = function () {
+  this._intervals = this._intervals || this.binary.split('').reduce(function (intervals, val, index) {
     if (val === '1') intervals.push(INTERVALS[index])
     return intervals
   }, [])
+  return this._intervals
+}
+
+Scale.prototype.rotations = function () {
+  this._rotations = this._rotations || buildRotations(this.binary.split(''))
+  return this._rotations
+}
+
+Scale.prototype.rotate = function (num) {
+  var count = this.rotations().length
+  return new Scale(this.rotations()[num % count])
+}
+
+Scale.prototype.reflection = function () {
+  return new Scale(this.binary.split('').reverse().join(''))
+}
+
+Scale.prototype.coscale = function () {
+  var array = this.binary.split('').map(function (char) {
+    return char === '0' ? '1' : '0'
+  })
+  for (var i = 0, len = array.length; i < len; i++) {
+    if (array[i] === '1') return new Scale(rotate(array, i, len).join(''))
+  }
+  throw Error('Something went wrong')
+}
+
+function buildRotations (array) {
+  var rotations = []
+  for (var i = 0, len = array.length; i < len; i++) {
+    rotations.push(rotate(array, i, len).join(''))
+  }
+  return rotations.filter(function (binary) {
+    return binary.charAt(0) === '1'
+  })
+}
+
+function rotate (array, num, len) {
+  len = len || array.length
+  return array.slice(num, len).concat(array.slice(0, num))
 }
--- test/binary.js
+++ test/binary.js
@@ -1,9 +1,62 @@
 var vows = require('vows')
 var assert = require('assert')
-var escalas = require('../')
+var Scale = require('../')
 
 vows.describe('Binary Scales').addBatch({
-  'testing': function () {
-    assert.deepEqual(escalas.build(2773, 'C'), ['c4', 'd4', 'e4', 'f4', 'g4', 'a4', 'b4'])
+  'constructor': {
+    'binary string': function () {
+      var s = new Scale('101011010101')
+      assert.equal(s.binary, '101011010101')
+    },
+    'integer': function () {
+      var s = new Scale(2773)
+      assert.equal(s.binary, '101011010101')
+    }
+  },
+
+  'rotations': {
+    topic: function () { return new Scale('1100') },
+
+    'rotations': function (s) {
+      var rotations = s.rotations()
+      assert.equal(rotations.length, 2)
+      assert.deepEqual(rotations, ['1100', '1001'])
+    },
+    'rotate': function (s) {
+      assert.equal(s.rotate(0).binary, '1100')
+      assert.equal(s.rotate(1).binary, '1001')
+    },
+
+    'major scale rotations': function () {
+      var major = new Scale(2773)
+      assert.equal(major.rotations().length, 7)
+      assert.deepEqual(major.rotations(), [
+        '101011010101', // ionian
+        '101101010110', // dorian
+        '110101011010', // phrygian
+        '101010110101', // lydian
+        '101011010110', // mixolydian
+        '101101011010', // aeolian
+        '110101101010'  // locrian
+      ])
+    }
+
+  },
+
+  'coscale': function () {
+    var major = new Scale(2773)
+    assert.equal(major.coscale().binary, '101001010100')
+  },
+
+  'reflection': function () {
+    var major = new Scale(2773)
+    assert.equal(major.reflection().binary, '101010110101')
+  },
+
+  'intervals': {
+    'major': function () {
+      var s = new Scale(2773)
+      assert.deepEqual(s.intervals(), ['P1', 'M2', 'M3', 'P4', 'P5', 'M6', 'M7'])
+    }
   }
 }).export(module)
