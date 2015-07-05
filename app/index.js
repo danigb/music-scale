70de6e18ab8caa5e437ddce88d1b2da2799581d3
danigb <danigb@gmail.com>
2015-07-05 02:42:27 +0200

    Router, part 1

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var riot = require('riot')
 var App = require('./app.js')
+var Scale = require('music-scale/all')
 
 // Tags
 var browser = require('./tags/browser.tag')
@@ -14,6 +15,21 @@ var app = new App({
 })
 
 riot.mount(browser, { app: app })
+
+riot.route(function (decimal, name) {
+  var scale
+  if (decimal && name) {
+    scale = Scale.get(+decimal)
+    app.select(scale.name() || scale.decimal)
+  } else if (decimal) {
+    scale = Scale.get(+decimal)
+    riot.route('' + scale.decimal + '/' + (scale.name() || 'unknown name'))
+  } else if (name) {
+    scale = Scale.get(name)
+    riot.route('' + scale.decimal + '/' + name)
+  } else {
+  }
+})
 
 setTimeout(function () {
   app.scales.build()
